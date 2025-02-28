ebafc690f2e1c5154ac747734aa25bd68b02ef46
CoolBoy0064 <70240755+CoolBoy0064@users.noreply.github.com>
2025-02-27 16:48:03 -0800

    Revert "Level Select Complete"
    This reverts commit ceecc5f2c5d7d3c9494d72291c445de04c5c0876.

--- ProjectileManage.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func handle_projectile_spawned(boolet, pos, direct, damage):
-	add_child(boolet)
-	boolet.global_rotation = direct.angle() + PI / 2.0
-	boolet.set_damage(damage)
-	boolet.global_position = pos
-	boolet.set_direction(direct)
