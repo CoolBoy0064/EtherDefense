d644e82562273ecb2ea46ae47ff7b669a2b01f68
vinnhhng <70670551+vinnhhng@users.noreply.github.com>
2025-04-27 20:18:08 -0700

    Add files via upload

--- EtherDefense/MainMenu.gd
+++ EtherDefense/MainMenu.gd
@@ -1,25 +1,20 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
 
 
 func _on_NewGameButton_pressed() -> void:
 	get_tree().change_scene_to_file("res://Level1.tscn")
-
-
+	
 func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_lvl_select_pressed() -> void:
 	get_tree().change_scene_to_file("res://LvlSelect.tscn")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Option Screen.tscn")
+###hello 
