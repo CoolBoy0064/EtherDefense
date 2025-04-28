e089c4dd8d0d0692e64c98a9d07ad4deaa2c64a7
vinnhhng <70670551+vinnhhng@users.noreply.github.com>
2025-04-27 20:10:48 -0700

    Add files via upload

--- EtherDefense/option_screen.gd
+++ EtherDefense/option_screen.gd
@@ -13,18 +13,18 @@ func _process(delta: float) -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Title Screen.tscn")
+	
+	
 
 func _on_option_button_item_selected(index: int) -> void:
 	match index:
 		0:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
-		1:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		2:
+		1:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
-		3: 
+		2: 
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MINIMIZED)
-		4: 
+		3: 
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 				
 #func _on_option_button_item_selected(index: int) -> void:
